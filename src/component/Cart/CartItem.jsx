14754138f07a92f0502102db2069fc1ef5510a8a
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-05-26 15:07:48 +0530

    add to cart fixed

--- src/component/Cart/CartItem.jsx
+++ src/component/Cart/CartItem.jsx
@@ -1,5 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { addItem, removeItem } from '../../cartSlice';
+
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { id, title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  const removeItemHandler = () => {
+    dispatch(removeItem(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(addItem({ id, title, price }));
+  };
 
   return (
     <li className="my-4 bg-gray-700 p-4 rounded">
@@ -15,10 +27,10 @@ const CartItem = (props) => {
           x <span className="text-lg font-bold">{quantity}</span>
         </div>
         <div className="flex justify-end mt-2">
-          <button className="bg-transparent border border-white ml-2 px-4 py-1 text-white hover:bg-gray-600">
+          <button onClick={removeItemHandler} className="bg-transparent border border-white ml-2 px-4 py-1 text-white hover:bg-gray-600">
             -
           </button>
-          <button className="bg-transparent border border-white ml-2 px-4 py-1 text-white hover:bg-gray-600">
+          <button onClick={addItemHandler} className="bg-transparent border border-white ml-2 px-4 py-1 text-white hover:bg-gray-600">
             +
           </button>
         </div>
