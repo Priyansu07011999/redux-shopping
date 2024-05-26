eae00fb0a00faedab1eb163a7fd13dcc79f26eee
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-05-26 15:07:11 +0530

    add to cart fixed

--- src/component/Cart/CartButton.jsx
+++ src/component/Cart/CartButton.jsx
@@ -1,8 +1,9 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggle } from '../../cartSlice';
 
-const CartButton = (props) => {
+const CartButton = () => {
   const dispatch = useDispatch();
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
     dispatch(toggle());
@@ -14,7 +15,7 @@ const CartButton = (props) => {
       className="bg-transparent border border-teal-400 text-teal-400 hover:text-white active:text-white flex items-center"
     >
       <span className="mx-2">My Cart</span>
-      <span className="bg-teal-400 rounded-full px-5 py-1 text-gray-900">1</span>
+      <span className="bg-teal-400 rounded-full px-5 py-1 text-gray-900">{totalQuantity}</span>
     </button>
   );
 };
