1361772bb920cccec2f27d257f85feac983cd0ad
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-05-26 15:09:19 +0530

    Update Products.jsx add to cart fixed

--- src/component/Shop/Products.jsx
+++ src/component/Shop/Products.jsx
@@ -1,15 +1,23 @@
 import ProductItem from './ProductItem';
 
 const Products = (props) => {
+  const products = [
+    { id: 'p1', title: 'Test', price: 6, description: 'This is a first product - amazing!' },
+  ];
+
   return (
     <section className="bg-gray-800 py-8">
       <h2 className="text-white my-8 text-center uppercase">Buy your favorite products</h2>
       <ul className="list-none p-0 m-0">
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
